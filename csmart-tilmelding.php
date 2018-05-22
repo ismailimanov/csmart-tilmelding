1bab9f40e2138bca0153130e7858f722bdd00b3d
ismailimanov <cph-ii20@cphbusiness.dk>
2018-05-22 15:39:20 +0200

    Plugin indhold

--- csmart-tilmelding.php
+++ csmart-tilmelding.php
@@ -7,3 +7,20 @@ Version: 1.0
 Author: Ismail og Kevin
 Author URI: https://imanov.io
 */
+
+add_action( 'admin_menu', 'csmart_tilmelding_menu' );
+
+/** Step 1. */
+function csmart_tilmelding_menu() {
+    add_options_page( 'test 1', 'Test 2', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+    if ( !current_user_can( 'manage_options' ) )  {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    echo '<div class="wrap">';
+    echo '<p>Indhold</p>';
+    echo '</div>';
+}
