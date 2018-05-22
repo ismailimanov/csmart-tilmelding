fd50c1d4d03bd03c5b5358cdcdfc3dcc74a10ec3
ismailimanov <cph-ii20@cphbusiness.dk>
2018-05-22 15:35:05 +0200

    Basis filer

---
+++ csmart-tilmelding.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: C-Smart Tilmelding
+Plugin URI: https://csmart.imanov.io
+Description: Tilmeldingssystem til C-Smart
+Version: 1.0
+Author: Ismail og Kevin
+Author URI: https://imanov.io
+*/
